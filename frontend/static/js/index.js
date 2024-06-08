a0c3e809ef628c3e956e755955ac63e4391c546e
Valerii <valerii.trebukh@gmail.com>
2024-06-08 16:44:35 +0300

    Custom router

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,30 @@
-console.log('Hello from SPA');
+const routes = [
+    { path: '/', view: () => viewHome() },
+    { path: '/dashboard', view: () => viewDashboard() },
+];
+
+
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+
+    let navTo = routes.find(route => route.path === location.pathname) || routes[0];
+    navigateTo(navTo);
+};
+
+function navigateTo({path, view}) {
+    history.pushState({}, "", path);
+    view();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    route();
+});
+
+function viewHome() {
+    document.getElementById('root').innerHTML = 'Home';
+}
+
+function viewDashboard() {
+    document.getElementById('root').innerHTML = 'Dashboard';
+}
